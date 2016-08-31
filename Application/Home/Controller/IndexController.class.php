32d8bcf36c96bb458e1a92d924d98f3e60a19c79
itman <2388408999@qq.com>
2016-08-31 15:57:46 +0800

    inset

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,61 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        vendor('Hprose.HproseHttpClient');
+        $client = new \HproseHttpClient('http://localhost/test/index.php/Home/Service/index');
+        $result = $client->test2();
+        dump($result);
+    }
+    /*
+    fsdfsdf
+    sfsdf
+    sdfsdf
+    sfdsdf
+    dfsfsdf
+     */
+    /**
+     * 
+     */
+/**
+ * 
+ * @return [type] [description]
+ */
+/**
+ * [setredis description]
+ * Typecho Blog Platform
+ * @copyright [copyright]
+ * @author 123123 <[<email address>]>
+ * @license  123123
+ * @version   [123123]
+ * @return    [t12312312ype]      [description]
+ */
+    public function setredis(){
+        $redis = new \Redis();
+    	$redis->connect('127.0.0.1', 6379);
+    	$redis->set('name_list4','bb1');
+    	// $redis->lpush('name_list3','cc2');
+    	// $redis->lpush('name_list3','cc3');
+    	// $redis->lpush('name_list3','cc4');
+    	if($redis->ping() == '+PONG'){
+    		echo 'ok';
+    	}
+    }
+
+    public function getredis(){
+        $redis = new \Redis();
+    	$redis->connect('127.0.0.1', 6379);
+    	// $rp = $redis->lrem('name_list3','cc4','1');
+    	// dump($rp);
+    	$re = $redis->lrange('name_list',0,-1);
+        dump($re);
+        // exit;
+    	echo '===============================';
+    	$br = $redis->brpoplpush('name_list','name_list2','3');
+    	dump($br);
+    	echo "===============================";
+    	$re = $redis->lrange('name_list2',0,-1);
+        dump($re);
+    }
+}
---
+++ Application/Home/Controller/ServiceController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller\HproseController;
+class ServiceController extends HproseController{
+    public function test1(){
+        return 'test1';
+    }
+    public function test2(){
+        return 'test2';
+    }
+}
